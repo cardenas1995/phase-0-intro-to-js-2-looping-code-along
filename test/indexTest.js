0c6ea5345e31a63fffd145666b040c8290a8a39a
Kevin <k.cardenas1795@gmail.com>
2024-01-19 17:03:47 -0500

    creates myfile.md

--- test/indexTest.js
+++ test/indexTest.js
@@ -50,3 +50,14 @@ describe( 'index.js', () => {
     } );
   } );
 } );
+
+
+function writeCards(names, giftType) {
+  return names.map(name => `Thank you, ${name}, for the wonderful ${giftType} gift!`);
+}
+
+function countDown(number) {
+  for (let i = number; i >= 0; i--) {
+    console.log(i);
+  }
+}
